4597f117960620628654dc2a01958bc9c895f2c1
choumar2001 <65606795+choumar2001@users.noreply.github.com>
2023-04-02 13:32:05 +0200

    add fake send email, store function, and read function

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 
 # this is used to make the script act as a webserver
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
 def scrape(url):
@@ -27,12 +28,29 @@ def extract(source):
     return value
 
 
+# send an email when there is upcoming tour
+def send_email():
+    print("Email was sent!")
 
+
+def store(extracted):
+    """Stores the data in a txt file"""
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read():
+    """Reads the data stored in the txt"""
+    with open("data.txt", "r") as file:
+        return file.read()
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
-    if extracted != "No Upcoming Tours":
-        send_email()
     print(extracted)
+    content = read()
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
